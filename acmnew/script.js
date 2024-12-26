09af35b33894852cdfe15c502c38cd464578458f
Aswanthwin Fried <110735877+aswanthwinfried@users.noreply.github.com>
2024-12-26 16:09:20 +0530

    Add files via upload

--- acmnew/script.js
+++ acmnew/script.js
@@ -4,3 +4,12 @@ const navList = document.querySelector('.navlist');
 menu.addEventListener('click', () => {
     navList.classList.toggle('show');
 });
+
+
+
+document.querySelector('.scroll-to-contact').addEventListener('click', function(e) {
+    e.preventDefault(); // Prevent default anchor behavior
+    document.querySelector('.contact-section').scrollIntoView({
+        behavior: 'smooth' // Smooth scrolling
+    });
+});
